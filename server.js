a4d8cea8878f9bba5b5f3b211ab3083eea144795
JKaur2716 <jaspreet814kaur@gmail.com>
2026-02-26 18:53:59 -0700

    App setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const helmet = require("helmet");
+const path = require("path");
+
+const app = express();
+
